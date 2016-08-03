b4f1939de0a11647ecee190502198e3566b545c1
Сергей Тулаев <odminchek@yandex.ru>
2016-08-03 13:27:03 +0400

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,26 +1,27 @@
 <?php
 
 //
-Route::get('/', function () {
-    return view('welcome');
+Route::get( '/', function () {
+    return view( 'welcome' );
 });
 
 // все категории
-Route::get('/api/categories/all', function () {
-    return 'All categories';
-});
+Route::get( '/api/categories/all', function () {
+    // return 'All categoies';
+
+} );
 
 // массив вебинаров указанной категории
-Route::get('/api/categories/webinars', function () {
+Route::get( '/api/categories/webinars', function () {
 	// /api/categories/webinars?alias=somethink
     return 'Webinars from category';
-});
+} );
 
 // один вебинар
-Route::get('/api/webinar/one', function () {
+Route::get( '/api/webinar/one', function () {
 	// /api/webinar/one?alias=somethink
     return 'One webinar';
-});
+} );
 
 
 
