6a2efa809a882a619a6a2ca8ed8b934a7f4d300d
Сергей Тулаев <odminchek@yandex.ru>
2016-08-04 13:01:20 +0400

    Пустые модели и контроллеры для вебинаров и тем

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Log;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\SubjectModel;
+
+class SubjectController extends Controller
+{
+    //
+}
