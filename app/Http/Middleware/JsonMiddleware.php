f78b0f5590e45f47567c2d6f6b1c3127f11e8c73
Сергей Тулаев <odminchek@yandex.ru>
2016-08-17 13:57:37 +0400

    fix

--- app/Http/Middleware/JsonMiddleware.php
+++ app/Http/Middleware/JsonMiddleware.php
@@ -14,11 +14,11 @@ class JsonMiddleware
     {
         if( !$request->has( 'body' )
             OR !is_string( $request->input( 'body' ) )
-            OR !json_decode( $request->input( 'body' ), TRUE )
+            OR !$body = json_decode( $request->input( 'body' ), TRUE )
             ):
             return response()->json( [ 'status' => 'error', 'HTTP' => '400 Bad Request' ] );
         endif;
 
-        return $next($request);
+        return $next( $request );
     }
 }
